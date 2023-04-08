e962b3774dae70d643eb5d3c4172faae4d5ab43a
Tamil mani <tmtamilmani700@gmail.com>
2023-04-08 11:10:53 +0530

    Array

---
+++ src/ArrayTraning.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class ArrayTraning {
+	
+	public static void main(String[] args) {
+		
+				Map<Integer,String> m = new LinkedHashMap<Integer,String>();
+m.put(null, null)				
+
+    
+
+Set<Entry<Integer, String>> entrySet = m.entrySet();
+
+
+  for (Entry<Integer, String> entry : entrySet) {
+	System.out.println(entry.getValue());
+}
+
+
+     {
+		
+	}
+	
+}
+
+
+}
+	
+	
+	
+}
+
+
